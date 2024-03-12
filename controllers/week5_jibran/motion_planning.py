4dc47954d5a38f491c9064954723b25d7b158988
Jibrangit <jibran.university@gmail.com>
2024-03-12 14:47:35 -0400

    Path is planned successfully

--- controllers/week5_jibran/motion_planning.py
+++ controllers/week5_jibran/motion_planning.py
@@ -3,6 +3,7 @@ import numpy as np
 from collections import deque, defaultdict
 from heapq import heapify, heappush, heappop
 import matplotlib.pyplot as plt
+import time
 
 
 def get_diagonal_neighbors(map: np.array, idx: t.Tuple) -> t.List:
@@ -11,7 +12,6 @@ def get_diagonal_neighbors(map: np.array, idx: t.Tuple) -> t.List:
     """
     width = len(map)
     height = len(map[0])
-    cell_cutoff = 0.3
 
     neighbor_indexes_costs = [
         (0, 1, 1),
@@ -33,7 +33,22 @@ def get_diagonal_neighbors(map: np.array, idx: t.Tuple) -> t.List:
     return neighbors
 
 
+def get_diagonal_distance(x1, y1, x2, y2):
+    dx = np.abs(x2 - x1)
+    dy = np.abs(y2 - y1)
+
+    if dy < dx:
+        return np.sqrt(2) * dy + (dx - dy)
+    else:
+        return np.sqrt(2) * dx + (dy - dx)
+
+
+def get_euclidean_distance(x1, y1, x2, y2):
+    return np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+
 def astar(map: np.array, start: t.Tuple, goal: t.Tuple) -> t.List[t.Tuple]:
+    start_time = time.time()
     visited = set()  # Set of Tuples
     graph = {}  # Dictionary of tuple (node) and list (parent)
 
@@ -53,7 +68,7 @@ def astar(map: np.array, start: t.Tuple, goal: t.Tuple) -> t.List[t.Tuple]:
         q,
         (
             distances[start]
-            + np.sqrt((goal[0] - start[0]) ** 2 + (goal[1] - start[1]) ** 2),
+            + get_euclidean_distance(goal[0], goal[1], start[0], start[1]),
             start,
         ),
     )
@@ -73,18 +88,18 @@ def astar(map: np.array, start: t.Tuple, goal: t.Tuple) -> t.List[t.Tuple]:
         curr = heappop(q)  # (Distance from start, node)
         curr_node = curr[1]
         if curr_node == goal:
-            print("Goal reached!!!!")
-            path_node = curr[1]
+            print(f"Path found in {time.time() - start_time} seconds!")
+            path_node = curr_node
             path = []
             while path_node != start:
                 path.append(path_node)
                 path_node = tuple(graph[path_node])
 
-                plt.plot(
-                    path_node[1], path_node[0], "r*"
-                )  # puts a red asterisk at the goal
-                plt.show()
-                plt.pause(0.000001)
+                # plt.plot(
+                #     path_node[1], path_node[0], "r*"
+                # )
+                # plt.show()
+                # plt.pause(0.000001)
 
             path.reverse()
             return path
@@ -101,34 +116,43 @@ def astar(map: np.array, start: t.Tuple, goal: t.Tuple) -> t.List[t.Tuple]:
                     graph[neighbor_idx] = list(curr_node)
                     distances[neighbor_idx] = cost
                     visited.add(neighbor_idx)
+                    heappush(
+                        q,
+                        (
+                            distances[neighbor_idx]
+                            + get_euclidean_distance(
+                                goal[0], goal[1], neighbor_idx[0], neighbor_idx[1]
+                            ),
+                            neighbor_idx,
+                        ),
+                    )
 
                 elif (neighbor_idx) in visited and cost < distances[neighbor_idx]:
                     graph[neighbor_idx] = list(curr_node)
                     distances[neighbor_idx] = cost
+                    heappush(
+                        q,
+                        (
+                            distances[neighbor_idx]
+                            + get_euclidean_distance(
+                                goal[0], goal[1], neighbor_idx[0], neighbor_idx[1]
+                            ),
+                            neighbor_idx,
+                        ),
+                    )
 
                 else:
                     continue
 
-                heappush(
-                    q,
-                    (
-                        distances[neighbor_idx]
-                        + np.sqrt(
-                            (goal[0] - neighbor_idx[0]) ** 2
-                            + (goal[1] - neighbor_idx[1]) ** 2
-                        ),
-                        neighbor_idx,
-                    ),
-                )
-
-        plt.plot(goal[1], goal[0], "y*")  # puts a yellow asterisk at the goal
-        plt.plot(curr_node[1], curr_node[0], "g*")
-        plt.show()
-        plt.pause(0.000001)
+        # plt.plot(goal[1], goal[0], "y*")  # puts a yellow asterisk at the goal
+        # plt.plot(curr_node[1], curr_node[0], "g*")
+        # plt.show()
+        # plt.pause(0.000001)
 
     print("Path to goal could not be found!!")
     return []
 
 
-map = np.load("cspace.npy")
-path = astar(map, (75, 75), (200, 200))
+if __name__ == "__main__":
+    map = np.load("cspace.npy")
+    path = astar(map, (75, 75), (200, 200))
--- controllers/week5_jibran/week5_jibran.py
+++ controllers/week5_jibran/week5_jibran.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 
 from bresenham import plot_line
 from robot_controller import Controller
+from motion_planning import astar
 
 
 LIDAR_NUM_READINGS = 667
@@ -28,95 +29,109 @@ MAP_LENGTH = 300
 KERNEL_SIZE = 56
 
 
-def world2map(x,y) -> Tuple[float]:
-    px = np.round(((x - TOP_LEFT_X) / ARENA_WIDTH)  * MAP_LENGTH)
+def world2map(x, y) -> Tuple[float]:
+    px = np.round(((x - TOP_LEFT_X) / ARENA_WIDTH) * MAP_LENGTH)
     py = np.round(((TOP_LEFT_Y - y) / ARENA_LENGTH) * MAP_LENGTH)
     return int(px), int(py)
+
 
 def map2world(px, py) -> Tuple[float]:
     x = ((px / MAP_LENGTH) * ARENA_WIDTH) + TOP_LEFT_X
     y = TOP_LEFT_Y - ((py / MAP_LENGTH) * ARENA_LENGTH)
     return x, y
 
+
 def compute_cspace(map):
-    kernel= np.ones((KERNEL_SIZE, KERNEL_SIZE))  
+    kernel = np.ones((KERNEL_SIZE, KERNEL_SIZE))
     cmap = map
-    cmap = signal.convolve2d(map,kernel,mode='same')
+    cmap = signal.convolve2d(map, kernel, mode="same")
     cmap = np.clip(cmap, 0, 1)  # As convolution increases values over 1.
     cspace = cmap > OCCUPANCY_GRID_THRESHOLD
     return cspace
-    
+
 
 def display_cspace(cspace):
     plt.imshow(cspace)
     plt.show()
 
-def save_cspace(cspace):
-    np.save('cspace',cspace)
 
-def plan_path(map, px, py):
-    pass
-    
+def save_cspace(cspace):
+    np.save("cspace", cspace)
+
+
+def plan_path(map, start, goal):
+    pstart = world2map(start[0], start[1])
+    pgoal = world2map(goal[0], goal[1])
+
+    path = astar(map, pstart, pgoal)
+    return [map2world(px, py) for px, py in path]
+
+
+def display_path(map, path):
+    pixel_path = [world2map(x, y) for x, y in path]
+    plt.imshow(map)
+    for p in pixel_path:
+        plt.plot(p[1], p[0], "r*")
+        plt.show()
+        plt.pause(0.000001)
+
 
 def main():
     robot = Supervisor()
-    
+
     # get the time step of the current world.
     timestep = int(robot.getBasicTimeStep())
-    
-    leftMotor = robot.getDevice('wheel_left_joint')
-    rightMotor = robot.getDevice('wheel_right_joint')
-    leftMotor.setPosition(float('inf'))
-    rightMotor.setPosition(float('inf'))
+
+    leftMotor = robot.getDevice("wheel_left_joint")
+    rightMotor = robot.getDevice("wheel_right_joint")
+    leftMotor.setPosition(float("inf"))
+    rightMotor.setPosition(float("inf"))
 
     # leftEncoder = robot.getDevice('wheel_left_joint_sensor')
     # rightEncoder = robot.getDevice('wheel_right_joint_sensor')
     # leftEncoder.enable(timestep)
     # rightEncoder.enable(timestep)
 
-    
-    lidar = robot.getDevice('Hokuyo URG-04LX-UG01')
+    lidar = robot.getDevice("Hokuyo URG-04LX-UG01")
     lidar.enable(timestep)
     lidar.enablePointCloud()
-    
-    gps = robot.getDevice('gps')
+
+    gps = robot.getDevice("gps")
     gps.enable(timestep)
-    
-    compass = robot.getDevice('compass')
+
+    compass = robot.getDevice("compass")
     compass.enable(timestep)
-    
-    angles = np.linspace(2*np.pi/3, -2*np.pi/3, LIDAR_NUM_READINGS)
+
+    angles = np.linspace(2 * np.pi / 3, -2 * np.pi / 3, LIDAR_NUM_READINGS)
     angles = angles[LIDAR_FIRST_READING_INDEX:LIDAR_LAST_READING_INDEX]
-    
+
     map = np.zeros((300, 300), dtype=float)
-    
+
     xr, yr = [], []
-    
-    map_display = robot.getDevice('map_display')
-    cspace_display = robot.getDevice('cspace_display')
-    
+
+    map_display = robot.getDevice("map_display")
+    cspace_display = robot.getDevice("cspace_display")
+
     home_position = (0.4, -3.1)
-    WP = [(-1.65, -3.2),
-          (-1.65, 0.35),
-          (0.65, 0.35),
-          (0.67, -1.65),
-          (0.56, -3.3)]
-    
+    WP = [(-1.65, -3.2), (-1.65, 0.35), (0.65, 0.35), (0.67, -1.65), (0.56, -3.3)]
 
     reverse_WP = deepcopy(WP)
     reverse_WP.reverse()
     reverse_WP.append(home_position)
-    
+
     marker = robot.getFromDef("marker").getField("translation")
 
-    controllers = [Controller(WHEEL_MAX_SPEED_RADPS, WP), Controller(WHEEL_MAX_SPEED_RADPS, reverse_WP)]
+    controllers = [
+        Controller(WHEEL_MAX_SPEED_RADPS, WP),
+        Controller(WHEEL_MAX_SPEED_RADPS, reverse_WP),
+    ]
     controller_idx = 0
     robot_coordinates = []
-    
+
     while robot.step(timestep) != -1:
-        xw    = gps.getValues()[0]
-        yw    = gps.getValues()[1]
-        theta = np.arctan2(compass.getValues()[0],compass.getValues()[1])
+        xw = gps.getValues()[0]
+        yw = gps.getValues()[1]
+        theta = np.arctan2(compass.getValues()[0], compass.getValues()[1])
 
         px_robot, py_robot = world2map(xw, yw)
         robot_coordinates.append((px_robot, py_robot))
@@ -124,13 +139,15 @@ def main():
         if controller_idx >= len(controllers):
             leftMotor.setVelocity(0.0)
             rightMotor.setVelocity(0.0)
-            break 
+            break
 
         elif controllers[controller_idx].completed():
             controller_idx += 1
             continue
 
-        marker.setSFVec3f([*controllers[controller_idx].get_current_target(), BALL_DIAMETER])
+        marker.setSFVec3f(
+            [*controllers[controller_idx].get_current_target(), BALL_DIAMETER]
+        )
 
         vl, vr = controllers[controller_idx].get_input_vels((xw, yw, theta))
 
@@ -140,16 +157,23 @@ def main():
         ranges = lidar.getRangeImage()
         ranges[ranges == np.inf] = 100
         ranges = ranges[LIDAR_FIRST_READING_INDEX:LIDAR_LAST_READING_INDEX]
-        
-        w_T_r = np.array([[np.cos(theta), -np.sin(theta), xw],
-                         [np.sin(theta),   np.cos(theta), yw],
-                         [0,                   0,         1]])
-        
-        
-        X_i = np.array([ranges * np.cos(angles) + LIDAR_ROBOT_X_OFFSET,
-                        ranges * np.sin(angles), 
-                        np.ones(LIDAR_ACTUAL_NUM_READINGS)])
-        
+
+        w_T_r = np.array(
+            [
+                [np.cos(theta), -np.sin(theta), xw],
+                [np.sin(theta), np.cos(theta), yw],
+                [0, 0, 1],
+            ]
+        )
+
+        X_i = np.array(
+            [
+                ranges * np.cos(angles) + LIDAR_ROBOT_X_OFFSET,
+                ranges * np.sin(angles),
+                np.ones(LIDAR_ACTUAL_NUM_READINGS),
+            ]
+        )
+
         X_w = w_T_r @ X_i
         for i in range(LIDAR_ACTUAL_NUM_READINGS):
             px, py = world2map(X_w[0][i], X_w[1][i])
@@ -163,9 +187,7 @@ def main():
                 py_laser = coordinate[1]
 
                 if map[px_laser, py_laser] > 0.01:
-                    map[px_laser, py_laser] -= 0.01 
-
-        
+                    map[px_laser, py_laser] -= 0.01
 
         # # Draw configuration map
         # for row in np.arange(0, MAP_LENGTH):
@@ -174,7 +196,7 @@ def main():
         #         if v > 0.01:
         #             map_display.setColor(v*256**2 + v*256 + v)
         #             map_display.drawPixel(row, col)
-        
+
         # # Draw configuration space
         # for row in np.arange(0, MAP_LENGTH):
         #     for col in np.arange(0, MAP_LENGTH):
@@ -184,14 +206,16 @@ def main():
         #         else:
         #             cspace_display.setColor(0x000000)
         #             cspace_display.drawPixel(row, col)
-        
+
         # cspace_display.setColor(0x00FF00)
         # for coordinate in robot_coordinates:
         #     cspace_display.drawPixel(coordinate[0], coordinate[1])
     cspace = compute_cspace(map)
-    display_cspace(cspace)
     save_cspace(cspace)
+    goal = (-1.65, 0.35)
+    path = plan_path(cspace, home_position, goal)  # Returns a path in world X-Y Positions
+    display_path(cspace, path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
